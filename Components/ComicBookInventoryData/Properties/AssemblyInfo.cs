f4af6768511396789089fe12b314618a64d8d659
migimasa <migimasa@gmail.com>
2016-08-03 21:30:53 -0500

    Create and Add Projects
    Create API project, Domain, and Data projects

--- Components/ComicBookInventoryData/Properties/AssemblyInfo.cs
+++ Components/ComicBookInventoryData/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ComicBookInventoryDomain")]
+[assembly: AssemblyTitle("ComicBookInventoryData")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ComicBookInventoryDomain")]
+[assembly: AssemblyProduct("ComicBookInventoryData")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,4 +20,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6943a53c-3201-46af-bedd-79a54bc9115e")]
+[assembly: Guid("011d3b26-eee8-4f55-aaf9-18ddf73a32ec")]
